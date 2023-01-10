194071ffcc3fc9ad1b14f175d870792b93c5d931
Derek Donkor <donkorderek@gmail.com>
2023-01-10 10:17:09 +0000

    Uploaded all files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gh/Documents/Derek/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gh/Documents/Derek/Apps/uenr_shop"
+export "FLUTTER_ROOT=C:\Users\Prof. Benjamin Emikp\A - Advance Coding - A\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Prof. Benjamin Emikp\Documents\GitHub\uenr_shop"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
